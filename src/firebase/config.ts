04e3b20da0cbff801635ace8ff0c9d839a2e3e54
pixom_ai <pixom.ai@gmail.com>
2026-02-22 12:03:42 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1862901099-45f44",
+  "appId": "1:576614825406:web:027ac1640cd39471d60def",
+  "apiKey": "AIzaSyCqOB3Ka2dA0Nl5kbLFrg9KER2whBkt3pA",
+  "authDomain": "studio-1862901099-45f44.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "576614825406"
+};
